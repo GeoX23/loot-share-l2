7b6d624266eb75b7f79821082edfa7ca70aff7e4
George Chervatidis <giwrgos.chervatidis@gmail.com>
2024-06-03 16:41:06 +0300

    ~hotfix Copy Button bg and footer text

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-slate-800 text-center pb-4 pt-4 border-t border-slate-600 fixed bottom-0 w-full">
+    <div className="bg-slate-800 text-center pb-4 pt-4 border-t border-slate-600 fixed bottom-0 w-full text-white">
       Page was created by{" "}
       <a
         href="https://github.com/GeoX23"
